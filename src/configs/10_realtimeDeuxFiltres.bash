178a4b8a84e7fc381c96a751478443d718ae94dd
Christian Gagné <christian.gagne@gel.ulaval.ca>
2021-03-05 16:30:38 -0500

    Update 10_realtimeDeuxFiltres.bash
    Correction option -s.

--- src/configs/10_realtimeDeuxFiltres.bash
+++ src/configs/10_realtimeDeuxFiltres.bash
@@ -5,16 +5,16 @@
 #   - Que les vidéos sont situés dans des dossiers *p (par exemple 160p) dans le même répertoire
 
 sudo rm /dev/shm/mem*           # On retire les identifiants des zones mémoire partagées des précédentes exécutions
-sudo ./decodeur -r FIFO 160p/02_Sintel.ulv /mem1 &
+sudo ./decodeur -s FIFO 160p/02_Sintel.ulv /mem1 &
 sudo ./decodeur 160p/02_Sintel.ulv /mem5 &
 sleep 0.5
-sudo ./convertisseur -r FIFO /mem1 /mem2 &
+sudo ./convertisseur -s FIFO /mem1 /mem2 &
 sudo ./convertisseur /mem5 /mem6 &
 sleep 0.5
-sudo ./filtreur -r FIFO -t 0 /mem2 /mem3 &
+sudo ./filtreur -s FIFO -t 0 /mem2 /mem3 &
 sudo ./filtreur -t 1 /mem6 /mem7 &
 sleep 0.5
-sudo ./redimensionneur -r FIFO -w 427 -h 240 -m 0 /mem3 /mem4 &
+sudo ./redimensionneur -s FIFO -w 427 -h 240 -m 0 /mem3 /mem4 &
 sudo ./redimensionneur -w 427 -h 240 -m 0 /mem7 /mem8 &
 sleep 0.5
 sudo ./compositeur /mem4 /mem8 &
